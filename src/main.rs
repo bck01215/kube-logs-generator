002613f97f8c49f9296460a98a73d9788c26fa03
Brandon Kauffman <bkauffman7@liberty.edu>
2022-10-31 08:10:14 -0400

    Fix stream issues
    Use a polling method to circumvent stream timeouts

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use kube_logs_generator::structures::{Condition, Pod, Pods};
 use lazy_static::lazy_static;
-use std::env;
-use std::fs;
-use std::fs::OpenOptions;
-use std::io::prelude::Write;
-use std::{thread, time};
+use std::{
+    env,
+    fs::{create_dir, File, OpenOptions},
+    io::{prelude::*, BufReader},
+    path::Path,
+    thread, time,
+};
 use tokio::task;
-
 lazy_static! {
     static ref TOKEN: String = env::var("KUBE_TOKEN").unwrap_or_default();
     static ref URL: String = env::var("KUBE_HOST").unwrap_or_default();
@@ -16,22 +17,16 @@ lazy_static! {
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let conditions = set_conditions();
     println!("Evaluating pods for {:?}", conditions);
-
-    let mut all_pods: Vec<Pod> = Vec::new();
-    let ten_secs = time::Duration::from_secs(60);
+    let secs = time::Duration::from_secs(10);
     loop {
         let pods = get_pods_with_names(&conditions).await?;
         for pod in pods.iter() {
-            let pod_clone = pod.clone();
-            if !all_pods.contains(pod) {
-                println!("Collecting for pod {}", pod.metadata.name);
-                all_pods.push(pod.clone());
-                task::spawn(async {
-                    let _ = handle_pod(pod_clone).await;
-                });
-            }
+            let clone_pod = pod.clone();
+            task::spawn(async {
+                let _ = handle_pod(clone_pod).await;
+            });
         }
-        thread::sleep(ten_secs);
+        thread::sleep(secs);
     }
 }
 
@@ -71,6 +66,7 @@ async fn get_pods_with_names(conditions: &Vec<Condition>) -> Result<Vec<Pod>, Bo
         .await?
         .json::<Pods>()
         .await?;
+
     'outer: for pod in resp.items.into_iter() {
         for cond in conditions {
             if (!cond.matching && pod.clone().metadata.labels.unwrap_or_default().get(&cond.key).unwrap_or(&"".to_string()) == &cond.value)
@@ -79,7 +75,6 @@ async fn get_pods_with_names(conditions: &Vec<Condition>) -> Result<Vec<Pod>, Bo
                 continue 'outer;
             }
         }
-        println!("Adding pod: {}", pod.metadata.name);
         pods.push(pod);
     }
     Ok(pods)
@@ -87,36 +82,43 @@ async fn get_pods_with_names(conditions: &Vec<Condition>) -> Result<Vec<Pod>, Bo
 
 async fn handle_pod(pod: Pod) -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
-    let mut resp = client
-        .get(URL.to_string() + &pod.metadata.self_link + "/log?timestamps=true&follow=true&tailLines=0")
+    let resp = client
+        .get(URL.to_string() + &pod.metadata.self_link + "/log?timestamps=true")
         .header("Authorization", "Bearer ".to_string() + &TOKEN)
         .send()
         .await?;
-    let _ = fs::create_dir("./logs/".to_owned() + &pod.metadata.namespace);
+    let _ = create_dir("./logs/".to_owned() + &pod.metadata.namespace);
     let file_name = &("./logs/".to_owned() + &pod.metadata.namespace + "/" + &pod.metadata.name + ".log");
-    'while_loop: while let Some(chunk) = resp.chunk().await? {
-        let s = match std::str::from_utf8(&chunk) {
-            Ok(v) => v,
-            Err(_e) => continue 'while_loop,
-        };
-        let end_stamp_index = s.find('Z').unwrap_or(0);
-        if s.len() < end_stamp_index + 2 {
+
+    let text = resp.text().await?;
+    let mut file = OpenOptions::new().write(true).append(true).create(true).read(true).open(file_name).unwrap();
+    let chunks = text.split('\n');
+    let cur_lines = lines_from_file(file_name);
+    'while_loop: for chunk in chunks {
+        let end_stamp_index = chunk.find('Z').unwrap_or(0);
+        if chunk.len() < end_stamp_index + 2 {
             continue 'while_loop;
         }
-        let timestamp = &s[..end_stamp_index + 1];
+        let timestamp = &chunk[..end_stamp_index + 1];
         let data = format!(
             r#"{{ "date": "{}", "msg": "{}", "pod": "{}", "namespace": "{}", "annotations": {:?} }}"#,
             timestamp,
-            &s[end_stamp_index + 2..].trim_end().replace('"', "'"),
+            &chunk[end_stamp_index + 2..].trim_end().replace('"', "'"),
             pod.metadata.name,
             pod.metadata.namespace,
             pod.clone().metadata.annotations.unwrap_or_default()
         );
-        let mut file = OpenOptions::new().write(true).append(true).create(true).open(file_name).unwrap();
-
-        if let Err(e) = writeln!(file, "{}", data) {
-            println!("Couldn't write to file: {}", e);
+        if !cur_lines.contains(&data) {
+            if let Err(e) = writeln!(file, "{}", data) {
+                println!("Couldn't write to file: {}", e);
+            }
         }
     }
     Ok(())
+}
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines().map(|l| l.expect("Could not parse line")).collect()
 }
